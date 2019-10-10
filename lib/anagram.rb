cc47cf7cf2e1e7d10eacf64e1cc258fdeaccbee1
James Benjamin <james.abenj@gmail.com>
2019-10-10 23:33:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+require "pry"
+class Anagram 
+  
+  attr_accessor :word
+  
+def initialize(word)
+  @word = word 
+end 
+
+def match(anagrams)
+  anagram = []
+ anagrams.each do |words|
+ array = @word.chars & words.chars
+ if array.size == @word.length 
+   anagram << words
+   binding.pry
+    end 
+  end 
+  anagram 
+end
+  
+  
+end 
